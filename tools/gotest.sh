37dcc986ccc2648d0d67894f86154f7bcb433d58
funjack <funjack@users.noreply.github.com>
2017-07-13 22:06:49 +0200

    Include build tools

---
+++ tools/gotest.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+echo "" > coverage.txt
+for d in $(go list ./... | egrep -v "(vendor|tools|contrib)"); do
+    go test -race -coverprofile=profile.out -covermode=atomic $d
+    if [ -f profile.out ]; then
+        cat profile.out >> coverage.txt
+        rm profile.out
+    fi
+done
